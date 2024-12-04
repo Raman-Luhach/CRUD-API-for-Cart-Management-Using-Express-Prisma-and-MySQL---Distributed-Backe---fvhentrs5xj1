d3528b04ddb4469ba7001f02b42ae499e250d46f
Raman-Luhach <ramanluhach369@gmail.com>
2024-12-04 16:07:14 +0530

    f

--- index.js
+++ index.js
@@ -26,14 +26,14 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-  connectDB()
+  // connectDB()
 });
 
-connectDB().then(()=>{
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-    })
-})
+// connectDB().then(()=>{
+//     app.listen(PORT, () => {
+//         console.log(`Server is running on port ${PORT}`);
+//     })
+// })
 
 
 module.exports = app;
--- middleware/authkey.js
+++ middleware/authkey.js
@@ -1,5 +1,5 @@
 // middleware/authMiddleware.js
-const API_AUTH_KEY = "8a60348b-d4a4-564a-9b45-aab518adb7f4";
+// const API_AUTH_KEY = "8a60348b-d4a4-564a-9b45-aab518adb7f4";
 
 const authMiddleware = (req, res, next) => {
     const apiauthkey = req.headers.apiauthkey;
@@ -8,7 +8,7 @@ const authMiddleware = (req, res, next) => {
         return res.status(403).json({ error: "apiauthkey is missing or invalid" });
     }
 
-    if (apiauthkey !== API_AUTH_KEY) {
+    if (apiauthkey !== process.env.API_AUTH_KEY) {
         return res.status(403).json({ error: "Failed to authenticate apiauthkey" });
     }
 
